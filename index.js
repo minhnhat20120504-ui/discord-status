394a60cd2acc8110285f5c79c05dacd5928bac86
MeMayBeo <minhnhat20120504@gmail.com>
2026-01-15 10:06:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.once("ready", () => {
     activities: [activities[0]]
   });
 
-  // đổi activity mỗi 10 giây
+  // đổi activity mỗi 5 giây
   setInterval(() => {
     index = (index + 1) % activities.length;
 
@@ -30,7 +30,8 @@ client.once("ready", () => {
       status: "online", // online | idle | dnd
       activities: [activities[index]]
     });
-  }, 10000);
+  }, 5000);
 });
 
 client.login(process.env.BOT_TOKEN);
+
