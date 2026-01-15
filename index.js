b8aa79e86de390f3ba071f8d778928274fa1cae1
MeMayBeo <minhnhat20120504@gmail.com>
2026-01-15 10:03:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
+const activities = [
+  { name: "pmnx.pages.dev", type: ActivityType.Playing },
+  { name: "phamminhnhat__", type: ActivityType.Watching },
+  { name: "[ HEAVEN IS HERE ]", type: ActivityType.Listening },
+  { name: "Discord.com", type: ActivityType.Playing }
+];
+
+let index = 0;
+
 client.once("ready", () => {
   console.log("Bot online:", client.user.tag);
 
+  // set ngay lần đầu
   client.user.setPresence({
-    status: "online", // online | idle | dnd | invisible
-    activities: [
-      {
-        name: "pmnx.pages.dev",
-        type: ActivityType.Playing
-      }
-    ]
+    status: "online",
+    activities: [activities[0]]
   });
+
+  // đổi activity mỗi 10 giây
+  setInterval(() => {
+    index = (index + 1) % activities.length;
+
+    client.user.setPresence({
+      status: "online", // online | idle | dnd
+      activities: [activities[index]]
+    });
+  }, 10000);
 });
 
 client.login(process.env.BOT_TOKEN);
